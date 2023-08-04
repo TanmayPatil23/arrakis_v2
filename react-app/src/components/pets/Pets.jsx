bb898e19847b423e7c3979c36f58e88002c034d2
Tanmay Patil <patilts19.comp@coep.ac.in>
2023-08-04 18:09:20 +0530

    Updating with dummy api for testing

--- react-app/src/components/pets/Pets.jsx
+++ react-app/src/components/pets/Pets.jsx
@@ -3,23 +3,24 @@ import { findPets } from "../../services/PetServices";
 import styles from "./Pets.module.css";
 
 export const Pets = () => {
-    const [pets, setPets] = useState([]);
+  const [pets, setPets] = useState([]);
 
-    useEffect(() => {
-      findPets()
-            .then(({data}) => {
-            setPets(data);
-            });
-    }, []);
+  useEffect(() => {
+    findPets().then(({ data }) => {
+      setPets(data);
+    });
+  }, []);
+  console.log(pets.icon_url);
   return (
     <>
-        { pets.map(pet => 
+      {/* { pets.map(pet => 
         <div className={styles.pets}>
             <div>ID: {pet.id}</div>
             <div>Name: {pet.name} </div>
             <div>Age: {pet.age}</div>
         </div>) 
-        }
+        } */}
+      <div className={styles.pets}>{JSON.stringify(pets)}</div>
     </>
-  )
+  );
 };
